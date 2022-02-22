2a44b27502a739c27008ed183b49a5b0ed67fed0
Thomas Kressman <thomas.kressman@gmail.com>
2022-02-22 17:59:12 -0500

    Implement logic for creating a suggestion

--- server/controllers/suggestions.js
+++ server/controllers/suggestions.js
@@ -3,17 +3,29 @@
 
 import SuggestionModel from "../models/suggestionModel.js";
 
-// Finding something inside of a model takes time, therefore this function is asynchronous
+/* Retrieve all suggestions */
 export const getSuggestions = async (req, res) => {
   try {
     const suggestions = await SuggestionModel.find();
     console.log(suggestions);
-    res.status(200).json(suggestions); // Everything is OK
+    res.status(200).json(suggestions); // OK
   } catch (error) {
-    res.status(404).json({ message: error.message }); // Everything is BAD
+    res.status(404).json({ message: error.message }); // Not found
   }
 };
 
-export const addSuggestion = (req, res) => {
-  res.send("SUGGESTION ADDED");
+/* Create a suggestion */
+export const addSuggestion = async (req, res) => {
+  // Retrieve suggestion data from the user
+  const suggestion = req.body;
+
+  // Create a new suggestion using the schema model
+  const newSuggestion = new SuggestionModel(suggestion);
+
+  try {
+    await newSuggestion.save();
+    res.status(201).json(newSuggestion); // Success in creating suggestion
+  } catch (error) {
+    res.status(409).json({ message: error.message }); // Conflict
+  }
 };
