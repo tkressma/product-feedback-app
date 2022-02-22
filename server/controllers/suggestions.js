5cfa95d2d1ed792ee116f05c270376356ca6387c
Thomas Kressman <thomas.kressman@gmail.com>
2022-02-22 17:50:09 -0500

    Implement logic for retrieving suggestions

--- server/controllers/suggestions.js
+++ server/controllers/suggestions.js
@@ -1,6 +1,19 @@
 /* This file contains all of the handlers (logic and functionality) for our routes.
    The purpose of having this file is to keep our code organized and maintainable. */
 
-export const getSuggestions = (req, res) => {
-  res.send("THIS WORKS!");
+import SuggestionModel from "../models/suggestionModel.js";
+
+// Finding something inside of a model takes time, therefore this function is asynchronous
+export const getSuggestions = async (req, res) => {
+  try {
+    const suggestions = await SuggestionModel.find();
+    console.log(suggestions);
+    res.status(200).json(suggestions); // Everything is OK
+  } catch (error) {
+    res.status(404).json({ message: error.message }); // Everything is BAD
+  }
+};
+
+export const addSuggestion = (req, res) => {
+  res.send("SUGGESTION ADDED");
 };
--- server/routes/suggestions.js
+++ server/routes/suggestions.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getSuggestions } from "../controllers/suggestions.js";
+import { getSuggestions, addSuggestion } from "../controllers/suggestions.js";
 
 // Initialize express router
 const router = express.Router();
 
 // Routes starting with '/suggestions'
 router.get("/", getSuggestions);
+router.post("/", addSuggestion);
 
 export default router;
