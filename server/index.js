1b19a4862e82f62102e3fe318065c67612032e79
Thomas Kressman <thomas.kressman@gmail.com>
2022-02-23 00:29:18 -0500

    Create .env file for environemnt variables

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import suggestionRoutes from "./routes/suggestions.js";
 
 // Initialize app
 const app = express();
+// Initialize environment variables
+dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -16,8 +18,7 @@ app.use("/suggestions", suggestionRoutes);
 
 // Connect app to MongoDB
 // Temporary database access before implementing environmental variables
-const CONNECTION_URL =
-  "mongodb+srv://project:project123@cluster0.7lz57.mongodb.net/project-feedback?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose
