3a1b881539c77304e3645b229b2048ade7e3e4bc
Thomas Kressman <thomas.kressman@gmail.com>
2022-02-22 21:48:11 -0500

    Add css reset.

--- client/src/components/Main/SuggestionsFeed/Suggestion/Suggestion.js
+++ client/src/components/Main/SuggestionsFeed/Suggestion/Suggestion.js
@@ -1,15 +1,23 @@
 import styles from "./Suggestion.module.css";
 import Tag from "../../../UI/Container/Tag/Tag";
+import commentIcon from "../../../../assets/shared/icon-comments.svg";
+import upvoteIcon from "../../../../assets/shared/icon-arrow-up.svg";
 export default function Suggestion() {
   return (
     <article className={styles.suggestion}>
-      <div className={styles["suggestion__upvote_count"]}>Upvote</div>
+      <div className={styles["suggestion__upvote"]}>
+        <img src={upvoteIcon} alt="Upvote arrow" />
+        <span>0</span>
+      </div>
       <section className={styles["suggestion__info"]}>
         <h3>Suggestion Name</h3>
         <p>Suggestion text message this is it.</p>
         <Tag>Feature</Tag>
       </section>
-      <div className={styles["suggestion__comment_count"]}>Comments</div>
+      <div className={styles["suggestion__comment_count"]}>
+        <img src={commentIcon} alt="A message bubble" />
+        <span>0</span>
+      </div>
     </article>
   );
 }
