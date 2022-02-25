4b62a102f1f3fb6295d56e38749ac7f949becf4c
Thomas Kressman <thomas.kressman@gmail.com>
2022-02-25 18:30:29 -0500

    Create upvote component

--- client/src/components/Main/SuggestionsFeed/Suggestion/Suggestion.js
+++ client/src/components/Main/SuggestionsFeed/Suggestion/Suggestion.js
@@ -1,7 +1,7 @@
 import styles from "./Suggestion.module.css";
 import Tag from "../../../UI/Tag/Tag";
+import UpvoteButton from "./UpvoteButton/UpvoteButton";
 import commentIcon from "../../../../assets/shared/icon-comments.svg";
-import upvoteIcon from "../../../../assets/shared/icon-arrow-up.svg";
 import Heading from "../../../UI/Heading/Heading";
 import TextBody from "../../../UI/TextBody/TextBody";
 export default function Suggestion({ suggestionData }) {
@@ -11,14 +11,7 @@ export default function Suggestion({ suggestionData }) {
   return (
     <article className={styles.suggestion}>
       {/* Upvote count */}
-      <button className={styles["suggestion__upvote"]}>
-        <img
-          src={upvoteIcon}
-          alt="Upvote arrow"
-          className={styles["suggestion__upvote_icon"]}
-        />
-        <span>{upvotes}</span>
-      </button>
+      <UpvoteButton upvotes={upvotes} />
 
       {/* Suggestion title, description, and tag */}
       <section className={styles["suggestion__info"]}>
---
+++ client/src/components/Main/SuggestionsFeed/Suggestion/UpvoteButton/UpvoteButton.js
@@ -0,0 +1,17 @@
+import styles from "./UpvoteButton.module.css";
+import upvoteIcon from "../../../../../assets/shared/icon-arrow-up.svg";
+
+const UpvoteButton = ({ upvotes }) => {
+  return (
+    <button className={styles["upvote"]}>
+      <img
+        src={upvoteIcon}
+        alt="Upvote arrow"
+        className={styles["upvote_icon"]}
+      />
+      <span>{upvotes}</span>
+    </button>
+  );
+};
+
+export default UpvoteButton;
