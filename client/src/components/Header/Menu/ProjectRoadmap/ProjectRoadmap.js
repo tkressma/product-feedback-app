633806c320ce76198ab4fb498dfb9a6aaf84a28b
Thomas Kressman <thomas.kressman@gmail.com>
2022-02-25 15:22:43 -0500

    Update styling for mobile menu

--- client/src/components/Header/Menu/ProjectRoadmap/ProjectRoadmap.js
+++ client/src/components/Header/Menu/ProjectRoadmap/ProjectRoadmap.js
@@ -1,4 +1,9 @@
 import styles from "./ProjectRoadmap.module.css";
+import Heading from "../../../UI/Heading/Heading";
 export default function ProjectRoadmap() {
-  return <article class={styles.roadmap}>Project roadmap</article>;
+  return (
+    <article class={styles.roadmap}>
+      <Heading type="h3">Project Roadmap</Heading>
+    </article>
+  );
 }
