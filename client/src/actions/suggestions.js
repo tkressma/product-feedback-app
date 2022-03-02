cb41f443e8a9263f342f459f20612e943c229865
Thomas Kressman <thomas.kressman@gmail.com>
2022-03-02 17:03:02 -0500

    Fix bug where suggestions bar will move. Refractor suggestions.js action and add comments

--- client/src/actions/suggestions.js
+++ client/src/actions/suggestions.js
@@ -1,8 +1,9 @@
 import { FETCH_ALL, FETCH_FILTERED } from "../constants/actionTypes";
 import * as api from "../api";
 
-// Action Creators
-// Must use redux thunk since we are working with asynchronous data
+// Action Creators - Must use redux thunk since we are working with asynchronous data
+
+// Returns all suggestions
 export const getSuggestions = () => async (dispatch) => {
   try {
     const { data } = await api.fetchSuggestions();
@@ -12,6 +13,10 @@ export const getSuggestions = () => async (dispatch) => {
   }
 };
 
+// Returns suggestions that meet filter criteria (category, type, and order)
+// Category - All, UI, UX, Enhancement, Bug, Feature
+// Type - Upvotes or Comments
+// Order - Ascending (Least) or Descending (Most)
 export const filterSuggestions =
   ({ category, type, order }) =>
   async (dispatch) => {
@@ -28,6 +33,10 @@ export const filterSuggestions =
     }
   };
 
+// Updates the filters currently selected by the user. This allows for
+// the search to work synchronously despite being in two seperate components.
+// Category component - SuggestionTag.js
+// Upvotes and Comments sort component - SuggestionsBar.js
 export const setFilters = (filters) => async (dispatch) => {
   dispatch({ type: "CHANGE_TYPE", payload: filters });
 };
