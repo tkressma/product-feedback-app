3dbb3c8b374aa546b1b8c34327ff59106f442ac6
Thomas Kressman <thomas.kressman@gmail.com>
2022-03-02 19:08:14 -0500

    Complete no suggestions found component

--- client/src/components/Main/SuggestionsFeed/SuggestionsFeed.js
+++ client/src/components/Main/SuggestionsFeed/SuggestionsFeed.js
@@ -1,15 +1,20 @@
 import styles from "./SuggestionsFeed.module.css";
 import { useSelector } from "react-redux";
 import Suggestion from "./Suggestion/Suggestion";
+import NoSuggestions from "./NoSuggestions.js/NoSuggestions";
 export default function SuggestionsFeed() {
   // Retrieves all of the suggestions
   const suggestions = useSelector((state) => state.suggestions);
 
-  return (
+  return suggestions.length !== 0 ? (
     <section className={styles.suggestions}>
       {suggestions.map((suggestion) => (
         <Suggestion key={suggestion["_id"]} suggestionData={suggestion} />
       ))}
+    </section>
+  ) : (
+    <section className={styles["suggestions--empty"]}>
+      <NoSuggestions />
     </section>
   );
 }
--- client/src/components/UI/Heading/Heading.js
+++ client/src/components/UI/Heading/Heading.js
@@ -1,8 +1,9 @@
 import styles from "./Headings.module.css";
 
-const Heading = ({ type, white, children }) => {
+const Heading = ({ type, white, headingTag, children }) => {
   // This allows us to set the HTML tag depending on the value of type.
-  const HeadingLevel = type;
+  // If headingTag is false, then it is simply a styled paragraph element.
+  const HeadingLevel = headingTag ? type : "p";
 
   // Allows us to select the CSS based on type value
   const headingStyle = `heading--${type}`;
