b7e10a575bb6a8ae637ac2004f656c6eddf5ab6b
Thomas Kressman <thomas.kressman@gmail.com>
2022-03-02 19:15:43 -0500

    Add more versatility to heading component

--- client/src/components/UI/Heading/Heading.js
+++ client/src/components/UI/Heading/Heading.js
@@ -1,9 +1,13 @@
 import styles from "./Headings.module.css";
 
-const Heading = ({ type, white, headingTag, children }) => {
+// This reusable heading takes in a few parameters:
+// type: The styling (and optionally tag) that the heading component is going to use (h1, h2, h3).
+// white: If true, the color of the font is white. Else, it is the default heading color.
+// headingTag: If true, use the associated heading tag determined by the value of type. Defaulted to true.
+const Heading = ({ type, white, headingTag = true, children }) => {
   // This allows us to set the HTML tag depending on the value of type.
   // If headingTag is false, then it is simply a styled paragraph element.
-  const HeadingLevel = headingTag ? type : "p";
+  const elementTag = headingTag ? type : "p";
 
   // Allows us to select the CSS based on type value
   const headingStyle = `heading--${type}`;
@@ -12,7 +16,7 @@ const Heading = ({ type, white, headingTag, children }) => {
   ${styles[headingStyle]} 
   ${white && styles.white}`;
 
-  return <HeadingLevel className={headingStyling}>{children}</HeadingLevel>;
+  return <elementTag className={headingStyling}>{children}</elementTag>;
 };
 
 export default Heading;
