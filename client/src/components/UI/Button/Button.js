e3f571563811a48e530ac54a55f9cec6dc178d81
Thomas Kressman <thomas.kressman@gmail.com>
2022-03-03 12:49:16 -0500

    Refractor Button component

--- client/src/components/UI/Button/Button.js
+++ client/src/components/UI/Button/Button.js
@@ -2,10 +2,11 @@ import styles from "./Button.module.css";
 import { Link } from "react-router-dom";
 
 export default function Button({ type, link = false, destination, children }) {
-  const ElementTag = link ? "a" : "button";
   const buttonType = `button--${type}`;
   const buttonStyling = `${styles.button} ${styles[buttonType]}`;
 
+  // If link is true, create a new styled router link to the given destination.
+  // Else, create a regular html button.
   if (link === true) {
     return (
       <Link to={destination} className={buttonStyling}>
