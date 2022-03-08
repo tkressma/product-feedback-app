9f0924d27c80407229dbe4eadbf499ad3b1f6a33
Thomas Kressman <thomas.kressman@gmail.com>
2022-03-08 12:24:58 -0500

    Add login button styling

--- client/src/components/Home/Header/Logo/Logo.js
+++ client/src/components/Home/Header/Logo/Logo.js
@@ -1,9 +1,22 @@
 import styles from "./Logo.module.css";
 export default function Logo() {
+  const user = true;
+
   return (
     <div className={styles.logo}>
-      <h1>Frontend Mentor</h1>
-      <p>Feedback Board</p>
+      <div className={styles["logo__text"]}>
+        <h1>Frontend Mentor</h1>
+        <p>Feedback Board</p>
+      </div>
+
+      {user ? (
+        <div className={styles["user__controls"]}>
+          <p>Welcome Tom</p>
+          <p>Logout</p>
+        </div>
+      ) : (
+        <p className={styles["logo__login"]}>Login</p>
+      )}
     </div>
   );
 }
