17f05d6e319c19fcb6c6e551c305db0de5d5adee
Thomas Kressman <thomas.kressman@gmail.com>
2022-03-09 23:18:43 -0500

    Remove unused imports

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,6 +1,3 @@
-import React, { useEffect } from "react";
-import { connectAdvanced, useDispatch } from "react-redux";
-import { getSuggestions } from "../../actions/suggestions";
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 
