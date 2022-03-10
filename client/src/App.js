f7ea922f977b89165af0d08590be6d1955892cdd
Thomas Kressman <thomas.kressman@gmail.com>
2022-03-09 23:19:26 -0500

    Remove unused import

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Create from "./components/Create/Create";
 import Container from "./components/UI/Container/Container";
