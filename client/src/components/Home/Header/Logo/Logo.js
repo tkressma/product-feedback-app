5274f512c419898832ca797d6761b160fe11ff1b
Thomas Kressman <thomas.kressman@gmail.com>
2022-03-17 20:21:35 -0400

    Add logout when user token expires

--- client/src/components/Home/Header/Logo/Logo.js
+++ client/src/components/Home/Header/Logo/Logo.js
@@ -3,17 +3,28 @@ import styles from "./Logo.module.css";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
+import decode from "jwt-decode";
 import LogoutButton from "../../../UI/LogoutButton/LogoutButton";
+import { useDispatch } from "react-redux";
 export default function Logo() {
   // Retrieve the user from localStorage
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
   const location = useLocation();
   const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
+  const dispatch = useDispatch;
+
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
 
   useEffect(() => {
     const token = user?.token;
 
-    // Check for JWT
+    // If the token has expired (1hr), logout
+    if (token) {
+      const decodedToken = decode(token);
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
 
     setUser(JSON.parse(localStorage.getItem("profile")));
   }, [location]);
