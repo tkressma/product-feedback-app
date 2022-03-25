8538f62f91fa49352d1f7bfee1f8da89caa0e77d
Thomas Kressman <thomas.kressman@gmail.com>
2022-03-25 18:08:27 -0400

    Create add comment components

--- client/src/components/SuggestionPage/SuggestionPage.js
+++ client/src/components/SuggestionPage/SuggestionPage.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./SuggestionPage.module.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import BackButton from "../UI/BackButton/BackButton";
 import Suggestion from "../Home/Main/SuggestionsFeed/Suggestion/Suggestion";
 import Comments from "./Comments/Comments";
+import { getSuggestion } from "../../actions/suggestions";
 export default function SuggestionPage() {
+  const dispatch = useDispatch();
   const { id } = useParams();
+  // Retrieve suggestion being requested via params id
+  useEffect(() => dispatch(getSuggestion(id)), []);
 
-  const { suggestions, isLoading } = useSelector((state) => state.suggestions);
-  // Find exact suggestion to be displayed
-  const suggestion = suggestions.find((suggestion) => suggestion._id === id);
+  const { suggestion, isLoading } = useSelector((state) => state.suggestions);
 
   return (
     <main className={styles["suggestion_page"]}>
