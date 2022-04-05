e4a8dfe67f5a14a1eeb9609a041b852fe05a047c
Thomas Kressman <thomas.kressman@gmail.com>
2022-04-05 16:21:30 -0400

    Add basic styling for roadmap mobile buttons

--- client/src/components/ProjectRoadmapPage/ProjectRoadmapPage.js
+++ client/src/components/ProjectRoadmapPage/ProjectRoadmapPage.js
@@ -3,8 +3,11 @@ import styles from "./ProjectRoadmapPage.module.css";
 import Heading from "../UI/Heading/Heading";
 import BackButton from "../UI/BackButton/BackButton";
 import Button from "../UI/Button/Button";
+import { useMediaQuery } from "react-responsive";
 
 export const ProjectRoadmapPage = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
+
   return (
     <main className={styles.roadmap}>
       <header className={styles["roadmap__header"]}>
@@ -23,7 +26,21 @@ export const ProjectRoadmapPage = () => {
         />
       </header>
 
-      <section></section>
+      <section>
+        {isMobile && (
+          <div className={styles["roadmap__options"]}>
+            <button
+              className={`${styles["roadmap__option"]} ${styles["roadmap__option--selected"]}`}
+            >
+              Planned (2)
+            </button>
+            <button className={styles["roadmap__option"]}>
+              In-Progress (3)
+            </button>
+            <button className={styles["roadmap__option"]}>Live (1)</button>
+          </div>
+        )}
+      </section>
     </main>
   );
 };
