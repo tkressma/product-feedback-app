fb03a23814c34419e25307478a1f33d6f432882d
Thomas Kressman <thomas.kressman@gmail.com>
2022-04-05 15:40:33 -0400

    Add planned roadmap bullets

--- client/src/components/Home/Header/Menu/ProjectRoadmap/ProjectRoadmap.js
+++ client/src/components/Home/Header/Menu/ProjectRoadmap/ProjectRoadmap.js
@@ -4,6 +4,30 @@ export default function ProjectRoadmap() {
   return (
     <article className={styles.roadmap}>
       <Heading type="h3">Project Roadmap</Heading>
+
+      <ul className={styles["roadmap__list"]}>
+        <li>
+          <div className={styles.roadmap__list_type}>
+            <div className={`${styles.bullet} ${styles.planned}`} /> Planned
+          </div>
+          <span className={styles.count}>2</span>
+        </li>
+
+        <li>
+          <div className={styles.roadmap__list_type}>
+            <div className={`${styles.bullet} ${styles.inprogress}`} /> In
+            Progress
+          </div>
+          <span className={styles.count}>3</span>
+        </li>
+
+        <li>
+          <div className={styles.roadmap__list_type}>
+            <div className={`${styles.bullet} ${styles.live}`} /> Live
+          </div>
+          <span className={styles.count}>1</span>
+        </li>
+      </ul>
     </article>
   );
 }
