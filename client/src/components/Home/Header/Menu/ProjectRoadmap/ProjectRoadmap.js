e62e610766ade5b3acbd52802b34ff03ddac7afd
Thomas Kressman <thomas.kressman@gmail.com>
2022-04-05 15:44:52 -0400

    Add view link to project roadmap component

--- client/src/components/Home/Header/Menu/ProjectRoadmap/ProjectRoadmap.js
+++ client/src/components/Home/Header/Menu/ProjectRoadmap/ProjectRoadmap.js
@@ -1,9 +1,15 @@
 import styles from "./ProjectRoadmap.module.css";
 import Heading from "../../../../UI/Heading/Heading";
+import { Link } from "react-router-dom";
 export default function ProjectRoadmap() {
   return (
     <article className={styles.roadmap}>
-      <Heading type="h3">Project Roadmap</Heading>
+      <div className={styles["roadmap__header"]}>
+        <Heading type="h3">Project Roadmap</Heading>
+        <Link to="/" className={styles["roadmap__link"]}>
+          View
+        </Link>
+      </div>
 
       <ul className={styles["roadmap__list"]}>
         <li>
